5ed13dc7cabac14649a08ff9fa8598439e1bc6e2
mayanja <mayanjer3@gmail.com>
2026-09-24 12:53:08 +0300

    Worked on the action decorator

--- store/views.py
+++ store/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework.mixins import DestroyModelMixin, ListModelMixin, RetrieveModelMixin, CreateModelMixin, UpdateModelMixin
 from store.serializers import *
 from store.models import Product, Collection, Cart
@@ -128,4 +128,12 @@ def collection_list(request): #function based view
 class CustomerViewSet(RetrieveModelMixin, UpdateModelMixin, CreateModelMixin, GenericViewSet):
      queryset = Customer.objects.all()
      serializer_class = CustomerSerializer
+     
+     @action(detail=False, methods=["GET", "PUT"])
+     def me(self, request):
+        (customer, created) = Customer.objects.get_or_create(user_id = request.user.id)
+        if request.method == "GET":
+            serializer = CustomerSerializer(customer)
+            return Response(serializer.data)
+        return Response(request.user.id)
      
